00197b7ee2cbf20b6633b883754be7c3a87685f2
JuanPablo Ramos <jprscs@gmail.com>
2018-08-25 13:18:06 -0500

    Created Login Storyboard and Constants file

---
+++ biblioTec/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  biblioTec
+//
+//  Created by Juan Pablo Ramos on 8/25/18.
+//  Copyright © 2018 Juan Pablo Ramos. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+	struct SegueIdenfiers {
+		static let toConfirmCode = "toConfirmCode"
+	}
+}
