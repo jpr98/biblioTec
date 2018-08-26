6bd9a1bd2a504efaf65e28be86e4dd5ba04516ee
JuanPablo Ramos <jprscs@gmail.com>
2018-08-25 20:34:47 -0500

    merge

---
+++ biblioTec/Controllers/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  biblioTec
+//
+//  Created by Ricardo Ramirez on 8/25/18.
+//  Copyright © 2018 Juan Pablo Ramos. All rights reserved.
+//
+
+import UIKit
+import AnimatedCollectionViewLayout
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var buscarLugarBtn: UIButton!
+    @IBOutlet weak var scanQrBtn: UIButton!
+    
+    private let animators: (LayoutAttributesAnimator, Bool, Int, Int) = (ZoomInOutAttributesAnimator(), true, 1, 1)
+                                                                           
+    
+    @IBAction func buscarLugarPressed(_ sender: Any) {
+        performSegue(withIdentifier: "toLookForPlace", sender: self)
+    }
+    
+    @IBAction func scanQrPressed(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        buscarLugarBtn.layer.cornerRadius = 10
+        scanQrBtn.layer.cornerRadius = 10
+    }
+    
+    
+}
